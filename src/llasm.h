5c15a072f233d4d63a1f5058df69b35a9f210f24
Tobias Weber <tobias.weber@tum.de>
2020-05-24 08:55:08 +0200

    initial commit

---
+++ src/llasm.h
@@ -0,0 +1,102 @@
+/**
+ * llvm three-address code generator
+ * @author Tobias Weber
+ * @date 11-apr-20
+ * @license: see 'LICENSE.GPL' file
+ *
+ * References:
+ *	* https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl03.html
+ *	* https://llvm.org/docs/GettingStarted.html
+ *	* https://llvm.org/docs/LangRef.html
+ */
+
+#ifndef __LLASM_H__
+#define __LLASM_H__
+
+#include "ast.h"
+#include "sym.h"
+
+
+class LLAsm : public ASTVisitor
+{
+public:
+	LLAsm(SymTab* syms, std::ostream* ostr=&std::cout);
+	virtual ~LLAsm() = default;
+
+
+	virtual t_astret visit(const ASTUMinus* ast) override;
+	virtual t_astret visit(const ASTPlus* ast) override;
+	virtual t_astret visit(const ASTMult* ast) override;
+	virtual t_astret visit(const ASTMod* ast) override;
+	virtual t_astret visit(const ASTPow* ast) override;
+	virtual t_astret visit(const ASTTransp* ast) override;
+	virtual t_astret visit(const ASTNorm* ast) override;
+	virtual t_astret visit(const ASTVar* ast) override;
+	virtual t_astret visit(const ASTCall* ast) override;
+	virtual t_astret visit(const ASTStmts* ast) override;
+	virtual t_astret visit(const ASTVarDecl* ast) override;
+	virtual t_astret visit(const ASTFunc* ast) override;
+	virtual t_astret visit(const ASTReturn* ast) override;
+	virtual t_astret visit(const ASTAssign* ast) override;
+	virtual t_astret visit(const ASTArrayAccess* ast) override;
+	virtual t_astret visit(const ASTArrayAssign* ast) override;
+	virtual t_astret visit(const ASTComp* ast) override;
+	virtual t_astret visit(const ASTCond* ast) override;
+	virtual t_astret visit(const ASTLoop* ast) override;
+	virtual t_astret visit(const ASTStrConst* ast) override;
+	virtual t_astret visit(const ASTNumConst<double>* ast) override;
+	virtual t_astret visit(const ASTNumConst<std::int64_t>* ast) override;
+	virtual t_astret visit(const ASTNumList<double>* ast) override;
+
+
+	// ------------------------------------------------------------------------
+	// internally handled dummy nodes
+	// ------------------------------------------------------------------------
+	virtual t_astret visit(const ASTArgNames*) override { return nullptr; }
+	virtual t_astret visit(const ASTArgs*) override { return nullptr; }
+	virtual t_astret visit(const ASTTypeDecl*) override { return nullptr; }
+	// ------------------------------------------------------------------------
+
+
+protected:
+	t_astret get_tmp_var(SymbolType ty = SymbolType::SCALAR,
+		const std::array<std::size_t, 2>* dims = nullptr,
+		const std::string* name = nullptr, bool on_heap = false);
+
+	/**
+	 * create a label for branch instructions
+	 */
+	std::string get_label();
+
+	/**
+	 * find the symbol with a specific name in the symbol table
+	 */
+	t_astret get_sym(const std::string& name) const;
+
+	/**
+	 * convert symbol to another type
+	 */
+	t_astret convert_sym(t_astret sym, SymbolType ty_to);
+
+	/**
+	 * get the corresponding data type name
+	 */
+	std::string get_type_name(SymbolType ty);
+
+
+private:
+	std::size_t m_varCount = 0;	// # of tmp vars
+	std::size_t m_labelCount = 0;	// # of labels
+
+	std::vector<std::string> m_curscope;
+
+	SymTab* m_syms = nullptr;
+
+	std::ostream* m_ostr = &std::cout;
+
+	// helper functions to reduce code redundancy
+	t_astret scalar_matrix_prod(t_astret scalar, t_astret matrix, bool mul_or_div=1);
+};
+
+
+#endif
